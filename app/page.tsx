b9db0d2523fd32736d5aa5381af1904fd2117fc2
Shehzad <shehzaddiqbal@gmail.com>
2023-02-13 23:28:24 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="w-full bg-gray p-5">
-      <nav className="flex justify-between items-center w-1200px m-auto">
+    <div className="bg-gray w-full p-5">
+      <nav className="w-1200px m-auto flex items-center justify-between">
         <h1 className="text-lg font-bold">Panaverse</h1>
         <ul className="flex gap-2">
           <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Home</li>
           <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">About</li>
           <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Contact</li>
-          <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Testimonal</li>
+          <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Testimanial</li>
         </ul>
       </nav>
     </div>
