6d3d98de66999876ada7a82d0b62c55361bc6b02
Shehzad <shehzaddiqbal@gmail.com>
2023-02-14 18:20:35 +0500

    tailwind theme file

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-
+import Navbar from "../components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="bg-gray w-full p-5">
-      <nav className="w-1200px m-auto flex items-center justify-between">
-        <h1 className="text-lg font-bold">Panaverse</h1>
-        <ul className="flex gap-2">
-          <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Home</li>
-          <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">About</li>
-          <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Contact</li>
-          <li className="pt-3 pb-3 pl-5 pr-5 hover:bg-gray-100">Testimanial</li>
-        </ul>
-      </nav>
-    </div>
-  );
+  return <Navbar />;
 }
