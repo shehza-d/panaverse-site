33f705f20e12a386bd4d8c32366a6642df429fb2
Shehzad <shehzaddiqbal@gmail.com>
2023-02-17 07:31:47 +0500

    font done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
 import "./globals.css";
+import { Outfit } from "@next/font/google";
+import { useState } from "react";
+
+const font = Outfit({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+  // variable: '--font-outfit', // optional for tailwind
+});
+
+const [themeMode, setThemeMode] = useState(true);
 
 export default function RootLayout({
   children,
@@ -6,13 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className=" bg-white dark:bg-bk">{children}</body>
+      <body className={`${themeMode ? "dark" : "light"}  bg-white dark:bg-bk`}>
+        {children}
+      </body>
     </html>
   );
 }
