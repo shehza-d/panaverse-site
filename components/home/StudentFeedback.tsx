c136cba70a680816dfc64cf9b47bc178983e498a
Shehzad <shehzaddiqbal@gmail.com>
2023-02-21 22:06:48 +0500

    why to join section done

---
+++ components/home/StudentFeedback.tsx
@@ -0,0 +1,9 @@
+import model from "../../public/home/model.png";
+
+export default function StudentFeedback() {
+  return (
+    <section className="h-screen  bg-white transition-all duration-300  dark:bg-bk">
+      <h2 className="text-5xl font-bold text-gray-100">StudentFeedback</h2>
+    </section>
+  );
+}
