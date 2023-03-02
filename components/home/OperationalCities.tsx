694681bb54da31094c9848791b23770baf0b6f56
Shehzad <shehzaddiqbal@gmail.com>
2023-03-02 17:55:45 +0500

    push

--- components/home/OperationalCities.tsx
+++ components/home/OperationalCities.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// import themeGradient from "../../public/theme-gradient.png";
 import karachi from "../../public/home/cities/karachi4.jpg";
 import lahore from "../../public/home/cities/lahore.png";
 import peshawar from "../../public/home/cities/peshawar.png";
